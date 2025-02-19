92730dad61cc617473aa534761cf6c176be72c4f
jdawgatl <140225820+jdawgatl@users.noreply.github.com>
2025-02-19 15:44:56 -0500

    corrected contactform

--- src/components/shared/ContactForm.tsx
+++ src/components/shared/ContactForm.tsx
@@ -164,7 +164,7 @@ const ContactForm = () => {
         <Checkbox id="consent" onCheckedChange={checked => setValue("consent", checked as boolean)} required />
         <label htmlFor="consent" className="text-sm text-gray-600 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
         I consent to receive calls, emails, and/or SMS/MMS for insurance related service and marketing purposes from Standard Financial Group, LLC, including the use of automated technology, artificial voice messages, or pre-recorded calls. Consent is not required to obtain any service or product. Message frequency varies and data rates may apply. Reply STOP to opt-out.
-        <Link to="/privacy" className="text-sky-600 hover:underline ml-1"> Privacy Policy</Link>
+        <Link to="/privacy" className="text-sky-600 hover:underline ml-1">Privacy Policy</Link>
         </label>
       </div>
 
